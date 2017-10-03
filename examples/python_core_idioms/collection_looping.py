1da850ec1e8dee92d9ad6c6f5299860886a79b55
tanukaaa <tanuka@mail.bg>
2017-10-03 14:56:27 +0300

    Added collection_looping.py file which is concerned with some of the built-in functions for list manipulation. NOTE# This file is unfinished!

---
+++ examples/python_core_idioms/collection_looping.py
@@ -0,0 +1,28 @@
+
+# As a person who come's from languages such as C# and Java, I misunderstood how the for-loop in python worked.
+
+# The misunderstood range function.
+
+# As a novice in python
+# I thought that
+#
+#                   for(int i = 0; i <=10; i++) {
+#                       #code here
+#                   }
+#
+
+# Is the same as
+#
+#                   for x in range(10):
+#                       #code here
+#
+
+
+# Naturally I was wrong. Therefore this led to, slow and ugly to read python code.
+
+
+# Let me show you what I mean:
+
+
+# How does one print the elemnts of a list using a for-loop.
+names = ['Jhonatan', 'Marrie', 'Steve', 'Ashley']
