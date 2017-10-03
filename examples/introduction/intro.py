a2f34ddf6f4428906de19f6cd2cec4e841a1f28d
tanukaaa <tanuka@mail.bg>
2017-10-03 13:46:50 +0300

    Added intro file including the scope of this project and a demo for the writing conventions in python.

---
+++ examples/introduction/intro.py
@@ -0,0 +1,22 @@
+# Transforming code into Beautiful python.
+
+# Hello there, this file is meant to be the cover of this project.
+# You will not find any python here just text.
+# And while you're here: - In python we use the '#' symbol for comments
+
+# The content of this project is as follows:
+
+# 1. A little about naming conventions
+
+# 2. Replacing All Traditional Index Manipulation with Python's core idioms.
+
+# 3. Learn Advanced techniques with for-else clauses and the two argument
+# form of iter()
+
+# 4. Improve your craftsmanship and aim for clean, fast,
+# idiomatic Python Code
+
+# Let's get started.
+
+
+
---
+++ examples/writing_conventions/writing_conventions_demo.py
@@ -0,0 +1,21 @@
+
+# A little about python's writing conventions.
+
+# Contrary to most programming languages (meaning the one's whom I have wrote code in)
+# python uses PEP 8, in other words, one should name his/her variables and methods using '_' (underscore).
+
+# There will be a link in the bottom of this file for the PEP 8 python code style guide.
+# In case you want to read more about it.
+
+
+# - Variables
+long_variable_name_in_python = 'Some String'
+
+
+# - Methods
+def some_method_name():
+    return "This method did something"
+
+
+# ? camelCase ?
+camelCase = 'ops not here.'
