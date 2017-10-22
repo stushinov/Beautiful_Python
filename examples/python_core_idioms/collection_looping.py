fdb9d41a0d8373c6b3891522e1c99a57f6468903
tanukaaa <tanuka@mail.bg>
2017-10-22 13:20:03 +0300

    Added information about the 'zip()' built-in function.

--- examples/python_core_idioms/collection_looping.py
+++ examples/python_core_idioms/collection_looping.py
@@ -68,14 +68,48 @@ for i in range(len(names)):
 # Let's see it in an example.
 
 
-# The right way
+# The correct way
 for i, name in enumerate(names):
     print(i, ' --> ', name)
 
 
 # Isn't the code more readable now ?
+# Yes it is and it is also faster !
 
 # ------------------------------------------------------------------------------ #
 
 
+# Let's get to the kinks. What if for example we had two lists (with different lengths)
+# and we had to iterate over them ?
 
+
+colors = ['red', 'blue', 'green', 'yellow']
+
+animals = ['Lion', 'Monkey', 'Platypus', 'Crocodile', 'Elephant']
+
+
+# The wrong way
+for i in range(min(len(colors), len(animals))):
+    print('The color is: ', colors[i], ' The animal is: ', animals[i])
+
+
+# The most obvious way to do this is by taking the lengths of the two collections,
+# compare them to see which one is smaller and then use it in the 'for' loop.
+
+# Isn't it ugly ? 'range(min(len(collection_A), len(collection_B)))'.
+# How about readability ? Performance ? Yes it is ugly, unreadable and slow,
+# so let's fix it by introducing the zip() function.
+
+
+# The correct way
+for color, animal in zip(colors, animals):
+    print('The color is: ', color, ' The animal is: ', animal)
+
+
+#TODO: Any grammar mistakes here ?
+
+# In a brief, what the 'zip()' function does is to take two collections
+# and start iterating over them. When there are no more values in one of the
+# two collections it just stops iterating. Pretty neat isn't.
+
+#TODO: Can zip take more than 2 collections ?
